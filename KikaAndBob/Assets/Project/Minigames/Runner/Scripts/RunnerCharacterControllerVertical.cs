ed5a0f86cf447dfd94050d8727fd1c1c2e4dec89
Robin Marx <robin@lugus-studios.be>
2014-01-02 17:20:19 +0100

    - Added parachute animations + SpeedChange (up + down arrows)  - started adding correct enemies and pickups

--- KikaAndBob/Assets/Project/Minigames/Runner/Scripts/RunnerCharacterControllerVertical.cs
+++ KikaAndBob/Assets/Project/Minigames/Runner/Scripts/RunnerCharacterControllerVertical.cs
@@ -4,18 +4,35 @@ using System.Collections.Generic;
 
 public class RunnerCharacterControllerVertical : LugusSingletonExisting<RunnerCharacterControllerVertical>, RunnerCharacterController
 {
+	public enum SpeedType
+	{
+		NONE = -1,
+
+		SLOW = 1, // modifierPercentage = 0
+		NORMAL = 2, // modifierPercentage = 0.5f
+		FAST = 3 // modifierPercentage = 1.0f
+	}
+
 	public int direction = -1; // -1 is down, 1 is up
 	public DataRange speedRange = new DataRange(13.0f, 26.0f);
+	public DataRange speedModifiers = new DataRange(0.5f, 1.5f);
 	public float timeToMaxSpeed = 60.0f;
 	public float horizontalSpeed = 4.0f;
 
 	[HideInInspector]
 	public float speedPercentage = 0.0f;
+	[HideInInspector]
+	public float speedModifierPercentage = 0.5f;
 
 	protected float startTime = -1.0f;
 
 	public delegate void OnHit(RunnerPickup pickup);
 	public OnHit onHit;
+	
+	public delegate void OnSpeedTypeChange(SpeedType oldType, SpeedType newType);
+	public OnSpeedTypeChange onSpeedTypeChange;
+
+	public SpeedType currentSpeedType = SpeedType.NORMAL;
 
 
 	public void OnPickupHit(RunnerPickup pickup)
@@ -59,23 +76,25 @@ public class RunnerCharacterControllerVertical : LugusSingletonExisting<RunnerCh
 		{
 			speedPercentage = timeDiff / timeToMaxSpeed;
 		}
+
+		float speedModifier = speedModifiers.ValueFromPercentage( speedModifierPercentage );
 		
 		// If the player's horizontal velocity is greater than the maxSpeed...
 		//if(Mathf.Abs(rigidbody2D.velocity.x) > speed)
 			// ... set the player's velocity to the maxSpeed in the x axis.
-		rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, speedRange.ValueFromPercentage(speedPercentage) * direction );
+		rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, speedRange.ValueFromPercentage(speedPercentage) * speedModifier * direction );
 
 		// Debug.LogWarning( rigidbody2D.velocity ); 
 
 
 		if( left )
 		{ 
-			this.rigidbody2D.velocity += Vector2.right * -1.0f * horizontalSpeed; 
+			this.rigidbody2D.velocity += Vector2.right * -1.0f * horizontalSpeed * speedModifier; 
 		}
 		
 		if( right )
 		{
-			this.rigidbody2D.velocity += Vector2.right  * horizontalSpeed; 
+			this.rigidbody2D.velocity += Vector2.right  * horizontalSpeed * speedModifier; 
 		}
 	}
 
@@ -101,5 +120,42 @@ public class RunnerCharacterControllerVertical : LugusSingletonExisting<RunnerCh
 		{
 			right = false;
 		}
+
+		CheckSpeedType();
+	}
+
+	protected void CheckSpeedType()
+	{
+		SpeedType targetType = SpeedType.NORMAL;
+		if( LugusInput.use.Key (KeyCode.UpArrow) )
+		{
+			targetType = SpeedType.SLOW;
+		}
+		else if( LugusInput.use.Key (KeyCode.DownArrow) )
+		{
+			targetType = SpeedType.FAST;
+		}
+
+		if( currentSpeedType != targetType )
+		{
+			SetSpeedType( targetType );
+		}
+	}
+
+	public void SetSpeedType(SpeedType type)
+	{
+		SpeedType oldType = currentSpeedType;
+		currentSpeedType = type;
+
+		if( currentSpeedType == SpeedType.NORMAL )
+			speedModifierPercentage = 0.5f;
+		else if( currentSpeedType == SpeedType.SLOW )
+			speedModifierPercentage = 0.0f;
+		else if( currentSpeedType == SpeedType.FAST )
+			speedModifierPercentage = 1.0f;
+
+
+		if( onSpeedTypeChange != null )
+			onSpeedTypeChange(oldType, type);
 	}
 }
