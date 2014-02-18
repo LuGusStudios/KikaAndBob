951a34a8a2fa2b24224704e609e85d4f9285ded1
Robin Marx <robin@lugus-studios.be>
2014-02-18 15:33:54 +0100

    - copied over new levels from Config to Resources  - added new pipeline for building webbuilds

--- KikaAndBob/Assets/Project/Editor/WebplayerBuilder.cs
+++ KikaAndBob/Assets/Project/Editor/WebplayerBuilder.cs
@@ -35,6 +35,195 @@ public class WebplayerBuilder : MonoBehaviour
 		EditorUtility.DisplayDialog("Scene list", output, "ok");
 	}
 
+	[MenuItem("KikaAndBob/Test Asset Moves")]
+	public static void AssetMoveTest ()
+	{
+		MoveSceneSpecificAssets( "/Resources", "/ResourcesTemp", "e03_china" );
+
+
+		/*
+		DirectoryInfo toplevel = new DirectoryInfo( Application.dataPath + "/Resources/" );
+
+		if( !Directory.Exists( Application.dataPath + tempFolder) )
+		{
+			Directory.CreateDirectory(Application.dataPath + tempFolder);
+		}
+
+
+		AssetMoveRecursive( toplevel, tempFolder, "e03_china" );
+		*/
+
+		/*
+		DirectoryInfo toplevel = new DirectoryInfo( Application.dataPath + "/" + tempFolder );
+		
+		if( !Directory.Exists( Application.dataPath + "/Resources") )
+		{
+			Directory.CreateDirectory(Application.dataPath + "/Resources");
+		}
+		
+		
+		AssetMoveRecursive( toplevel, "/Resources", "e03_china" );
+		*/
+		
+		EditorUtility.DisplayDialog("Moving assets", "Done", "ok"); 
+	}
+	
+	
+	[MenuItem("KikaAndBob/Test Asset Moves Back")]
+	public static void AssetMoveTestBack () 
+	{
+		MoveSceneSpecificAssets( "/ResourcesTemp", "/Resources", "e03_china" );
+		
+		EditorUtility.DisplayDialog("Moving assets back", "Done", "ok"); 
+	}
+
+	protected static void MoveSceneSpecificAssets( string sourceFolder, string targetFolder, string excludePrefix )
+	{
+		List<string> l = new List<string>();
+		l.Add( excludePrefix );
+
+		MoveSceneSpecificAssets( sourceFolder, targetFolder, l );
+	}
+
+	protected static void MoveSceneSpecificAssets( string sourceFolder, string targetFolder, List<string> excludePrefixes )
+	{
+		string excluded = "";
+		foreach( string prefix in excludePrefixes )
+			excluded += "" + prefix + ", ";
+
+		UnityEngine.Debug.Log("MoveSceneSpecificAssets: " + sourceFolder + " to " + targetFolder + " without " + excluded);
+
+		depth = 0;
+		
+		DirectoryInfo source = new DirectoryInfo( Application.dataPath + sourceFolder );
+		
+		if( source == null )
+		{
+			UnityEngine.Debug.LogError("Source folder does not exist! " + (Application.dataPath + sourceFolder) );
+			return;
+		}
+		
+		if( !Directory.Exists( Application.dataPath + targetFolder ) )
+		{
+			Directory.CreateDirectory(Application.dataPath + targetFolder);
+		}
+		
+		
+		AssetMoveRecursive( source, targetFolder, excludePrefixes );
+	}
+
+	public static int depth = 0;
+	protected static void AssetMoveRecursive(DirectoryInfo parent, string basePath, List<string> excludePrefixes)
+	{
+		if( depth > 20 ) 
+			return;
+
+		// recursive in side directories
+		DirectoryInfo[] dirs = parent.GetDirectories();
+		foreach( DirectoryInfo dir in dirs )
+		{
+			if( !Directory.Exists( Application.dataPath + basePath + "/" + dir.Name ) )
+			{
+				Directory.CreateDirectory( Application.dataPath + basePath + "/" + dir.Name);
+			}
+
+			++depth;
+
+			UnityEngine.Debug.Log("MoveAssets: Recursiving " + (basePath + "/" + dir.Name) ); 
+			AssetMoveRecursive( dir, basePath + "/" + dir.Name, excludePrefixes );
+
+			/*
+			DirectoryInfo[] sceneFolders = category.GetDirectories();
+			foreach( DirectoryInfo sceneFolder in sceneFolders )
+			{
+				FileInfo[] scenes = sceneFolder.GetFiles("*.unity");
+				foreach(FileInfo scene in scenes )
+				{
+					string name = scene.FullName.Replace( Application.dataPath, "Assets");
+					
+					//output += "levels.Add(\"" + name + "\");\n"; 
+				}
+			}
+			*/
+		}
+
+		FileInfo[] files = parent.GetFiles();
+		foreach( FileInfo file in files )
+		{
+			// for MOveAssets, we need the path relative to the PROJECT folder of unity...
+			// we don't have that readily available, so force it out of file.FullName
+			// App.DataPath is everything up and including Assets, so we need to prepend assets ourselves
+			string oldPath = "Assets" + (file.FullName.Replace(Application.dataPath, ""));
+
+			string newPath = "Assets" + basePath + "/" + file.Name;
+
+			if( file.Name.Contains(".meta") )
+			{
+				continue;
+			}
+
+			bool prefixFound = false;
+			foreach( string prefix in excludePrefixes )
+			{
+				//UnityEngine.Debug.LogWarning("Checking file " + file.Name +" to prefix " + prefix + " -> " + file.Name.Contains(prefix) );
+				if( file.Name.Contains(prefix) )
+				{
+					prefixFound = true;
+					break;
+				}
+			}
+
+			if( prefixFound )
+				continue;
+
+			// if we get here, the file doesn't contain prefix
+			// still, we can't necessarily just move it. This would mean missing all normal SFX and buttons etc.
+			// we ONLY want to move stuff that is scene specific, i.e. exx_
+			// so check if we have that kind of file
+			if( !file.Name.StartsWith("e") )
+				continue;
+
+			if( file.Name.IndexOf("_") != 3 )
+				continue;
+
+			string numbers = file.Name.Substring(1, 2);
+			int epnumber = 0;
+
+			try
+			{
+				epnumber = int.Parse(numbers);
+			}
+			catch( System.Exception e ) 
+			{
+				// if this was no number, it's no episode-file, so we skip it;
+				UnityEngine.Debug.LogWarning("File skipped : " + file.Name);
+				continue;
+			}
+
+
+			// DEBUG : remove me
+			//if( !file.FullName.Contains("Audio") )
+			//{
+			//	continue;
+			//}
+			                       
+			
+			// DEBUG : remove me
+			//if( file.Name.Contains("e03_china_1.") )
+			//{
+				UnityEngine.Debug.LogError("Moving file " + oldPath + " TO " + newPath );
+				string result = AssetDatabase.MoveAsset( oldPath, newPath );
+
+				if( !string.IsNullOrEmpty(result) )
+				{
+					UnityEngine.Debug.LogError("Error moving " + oldPath + " TO " + newPath + " -> " + result);
+				}
+			//}
+
+			//MoveAsset( "/Assets" + basePath );
+		}
+	}
+
 	[MenuItem("KikaAndBob/Build for web")]
 	public static void BuildGame ()
 	{
@@ -87,9 +276,14 @@ public class WebplayerBuilder : MonoBehaviour
 			level = level.Substring(level.LastIndexOf("/") + 1);
 			level = level.Replace(".unity", "");
 
+
+			MoveSceneSpecificAssets( "/Resources", "/ResourcesTemp", level );
+
 			string[] lvl = new string[1]; 
 			lvl[0] = currentLevel;
 			BuildPipeline.BuildPlayer(lvl, path + "/" + level, BuildTarget.WebPlayer, BuildOptions.None);
+			
+			MoveSceneSpecificAssets( "/ResourcesTemp", "/Resources", level );
 		}
 
 
