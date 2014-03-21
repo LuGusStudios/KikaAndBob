1af670c57e91f4c6e30772c66f3067cb8b070b1e
Robin Marx <robin@lugus-studios.be>
2014-03-21 15:46:43 +0100

    - Added MJ to Israel (still needs billy jean though)  - New background music for israel

---
+++ KikaAndBob/Assets/Scenes/Minigames/e16_israel/Scripts/MJ.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MJ : MonoBehaviour 
+{
+	public void SetupLocal()
+	{
+		// assign variables that have to do with this class only
+	}
+	
+	public void SetupGlobal()
+	{
+		// lookup references to objects / scripts outside of this script
+
+		gameObject.MoveTo( this.transform.position.x ( this.transform.position.x - 3000 ) ).Time (7.0f).Execute();
+	}
+	
+	protected void Awake()
+	{
+		SetupLocal();
+	}
+
+	protected void Start () 
+	{
+		SetupGlobal();
+	}
+	
+	protected void Update () 
+	{
+	
+	}
+}
