b96f26db1ec6805e0d3455e6b7e5d163ac85cf66
Kasper Adriaensen <kasper@lugus-studios.be>
2014-07-25 14:46:18 +0200

    - Made action button for Sicily. Had to reposition pause button for this too.

--- KikaAndBob/Assets/Scenes/Minigames/e25_sicily/Scripts/PacmanSicilyGUIConfig.cs
+++ KikaAndBob/Assets/Scenes/Minigames/e25_sicily/Scripts/PacmanSicilyGUIConfig.cs
@@ -8,5 +8,22 @@ public class PacmanSicilyGUIConfig : IPacmanSpecificConfig
 	{
 		HUDManager.use.GetElementForCommodity(KikaAndBob.CommodityType.Life).gameObject.SetActive(false);
 		HUDManager.use.CounterSmallRight1.commodity = KikaAndBob.CommodityType.Dynamite;
+		HUDManager.use.RepositionPauseButton(KikaAndBob.ScreenAnchor.Top, KikaAndBob.ScreenAnchor.Top);
+
+		DirectionPad dp = (DirectionPad) FindObjectOfType(typeof(DirectionPad));
+
+		if (dp != null)
+		{
+			PacmanDynamite[] dynamiteInLevel = (PacmanDynamite[]) FindObjectsOfType(typeof(PacmanDynamite));
+		
+			if (dynamiteInLevel.Length >= 1)
+			{
+				dp.transform.FindChild("Button1").gameObject.SetActive(true);
+			}
+			else
+			{
+				dp.transform.FindChild("Button1").gameObject.SetActive(false);
+			}
+		}
 	}
 }
